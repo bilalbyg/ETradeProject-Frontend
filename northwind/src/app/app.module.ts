6c148ac382fcaccd908bc2ef839965a64cf0a4ce
Bilal <bbeygo.71@gmail.com>
2021-03-31 15:28:49 +0300

    interceptors added

--- northwind/src/app/app.module.ts
+++ northwind/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
@@ -15,6 +15,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +39,9 @@ import { LoginComponent } from './components/login/login.component';
       positionClass : "toast-bottom-right"
     })
   ],
-  providers: [],
+  providers: [
+    {provide : HTTP_INTERCEPTORS, useClass : AuthInterceptor,multi : true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
